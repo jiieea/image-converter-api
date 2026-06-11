695d1c8086e5b8c8062d7ea3adaf43abb158c098
jiieea <seigneur.noir15@gmail.com>
2026-06-11 13:42:11 +0700

    deploying config

--- src/main.ts
+++ src/main.ts
@@ -5,6 +5,7 @@ import { WINSTON_MODULE_NEST_PROVIDER } from 'nest-winston';
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
   const logger = app.get(WINSTON_MODULE_NEST_PROVIDER);
+  app.enableCors();
   app.useLogger(logger);
   await app.listen(process.env.PORT ?? 3000);
 }
